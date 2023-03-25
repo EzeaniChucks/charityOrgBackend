2ccec199e26243f6714e4b582ec05c2d634e0de9
EzeaniChucks <concord_chucks2@yahoo.com>
2023-03-25 14:58:59 -0700

    unified all response json syntax

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,55 +3,51 @@ const User = require('../schema/userSchema')
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
-const register = async (req, res)=>{
-    const {
-      password,
-      phoneNumber,
-      dateOfBirth,
-      cvv,
-      expirationDate,
-    } = req.body;
-    
-    if(!password){
-        return res.status(400).json('incomplete credentials')
-    }
+const register = async (req, res) => {
+  const { password, phoneNumber, dateOfBirth, cvv, expirationDate } = req.body;
 
-    try{
-        const salt = await bcrypt.genSalt(10);
-        const hashedpass = await bcrypt.hash(password, salt);
-        
-        const verificationToken = crypto.randomBytes(40).toString('hex');
-        
-        const user = await User.create({
-          ...req.body,
-          cvv: Number(cvv),
-          expirationDate: new Date(expirationDate),
-          dateOfBirth: new Date(dateOfBirth),
-          phoneNumber: Number(phoneNumber),
-          password: hashedpass,
-          verificationToken,
-        });
-    
-        if(!user){
-            return res.status(400).json('Invalid credentials')
-        }
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            type:'OAuth2',
-            user: process.env.USER,
-            pass: process.env.PASS,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
-          },
-        });
-        
-        await transporter.sendMail({
-          from: '"charity Org" <charityapplicationmail@gmail.com>',
-          to: `${user.email}`,
-          subject: "CharityOrg: Account verification:",
-          html: `
+  if (!password) {
+    return res.status(400).json({ msg: "incomplete credentials" });
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpass = await bcrypt.hash(password, salt);
+
+    const verificationToken = crypto.randomBytes(40).toString("hex");
+
+    const user = await User.create({
+      ...req.body,
+      cvv: Number(cvv),
+      expirationDate: new Date(expirationDate),
+      dateOfBirth: new Date(dateOfBirth),
+      phoneNumber: Number(phoneNumber),
+      password: hashedpass,
+      verificationToken,
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "Something went wrong. Please try again" });
+    }
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.USER,
+        pass: process.env.PASS,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+      },
+    });
+
+    await transporter.sendMail({
+      from: '"charity Org" <charityapplicationmail@gmail.com>',
+      to: `${user.email}`,
+      subject: "CharityOrg: Account verification:",
+      html: `
                 <h3>Hello ${user.firstName}</h3>
                 <h4>Welcome to Charity.Org</h4>
                 <p>You are almost done. Kindly verify your account</p>
@@ -61,60 +57,69 @@ const register = async (req, res)=>{
                 >
                 Verify account
                 </a>`,
-        });
-        
-        return res.status(200).json({msg:'Success! We sent you an email to verify your account'})
-    }catch(err){
-        return res.status(400).json({msg:err.message})
+    });
+
+    return res
+      .status(200)
+      .json({ msg: "success! we sent you an email to verify your account" });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res
+      .status(400)
+      .json({ msg: "No user with this email. Please try registering" });
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    return res.status(400).json({ msg: "false or expired token" });
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+  try {
+    await user.save();
+    return res.status(200).json({ msg: "email verified", user });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { password, email } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Incomplete credentials" });
+  }
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "This user does not exist. Try registering" });
     }
-}
 
-const verifyEmail = async(req, res)=>{
-    const {verificationToken, email} = req.body;
-    const user = await User.findOne({email});
-    if(!user){
-       return res.status(400).json({msg:'No user with this email. Please try registering'})
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+
+    if (isPassCorrect) {
+      if (user.isVerified) {
+        return res.status(200).json({ msg: "success", user });
+      } else {
+        return res.status(400).json({ msg: "Please verify your email" });
+      }
+    } else {
+      return res.status(400).json({ msg: "wrong email or password" });
     }
-
-    if(user.verificationToken !== verificationToken){
-       return res.status(400).json({msg:'false or expired token'})
-    }
-    
-    user.isVerified = true;
-    user.verified = Date.now();
-    user.verificationToken = '';
-
-    await user.save()
-
-    return res.status(200).json({msg:'email verified', user})
-}
-
-const login = async(req, res)=>{
-    const{password, email} = req.body;
-    try{
-        
-        const user = await User.findOne({email})
-        
-        if(!user){
-           return res.status(400).json({msg:'This user does not exist. Try registering'})
-        }
-
-        const isPassCorrect = await bcrypt.compare(password, user.password)
-        
-        if(isPassCorrect){
-            if(user.isVerified){
-               return res.status(200).json({ msg: "success", user });
-            } else{
-               return res.status(400).json({msg:'Please verify your email'})
-            }
-        } else{
-            return res.status(400).json({msg:'wrong email or password'})
-        }
-
-    }catch(err){
-        return res.status(400).json(err.message)
-    }
-}
+  } catch (err) {
+    return res.status(400).json({ msg: err.message });
+  }
+};
 
 module.exports = {login, register, verifyEmail}
 
